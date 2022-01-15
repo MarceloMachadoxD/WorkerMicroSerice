31104a5b6b0388010a37dfcc9dee22c24d04364f
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2022-01-15 15:55:39 -0300

    HR Worker created

---
+++ hr-workerms/src/main/java/com/github/marcelomachadoxd/hrworkerms/HrWorkermsApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcelomachadoxd.hrworkerms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkermsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrWorkermsApplication.class, args);
+    }
+
+}
---
+++ hr-workerms/src/test/java/com/github/marcelomachadoxd/hrworkerms/HrWorkermsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcelomachadoxd.hrworkerms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkermsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
