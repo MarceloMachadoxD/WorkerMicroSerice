cb3c408cdaf1910c2c032b75dc53f30d49945634
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2022-01-15 16:47:13 -0300

    HRPayrollMS implementation

---
+++ Payroll/src/test/java/com/github/marcelomachadoxd/hrpayrollms/PayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcelomachadoxd.hrpayrollms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
