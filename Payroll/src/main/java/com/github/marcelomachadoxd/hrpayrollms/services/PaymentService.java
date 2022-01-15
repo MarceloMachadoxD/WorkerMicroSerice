738f940d2dea7747166afcba3f63f6c695ec67fb
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2022-01-15 17:27:03 -0300

    Rest Template

--- Payroll/src/main/java/com/github/marcelomachadoxd/hrpayrollms/services/PaymentService.java
+++ Payroll/src/main/java/com/github/marcelomachadoxd/hrpayrollms/services/PaymentService.java
@@ -1,13 +1,36 @@
 package com.github.marcelomachadoxd.hrpayrollms.services;
 
 import com.github.marcelomachadoxd.hrpayrollms.entities.Payment;
+import com.github.marcelomachadoxd.hrpayrollms.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hrworker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(Long workerId, Integer days) {
-        return new Payment("Bob", 200.0d, days); //temporary mock
+
+        Map<String, String> uriVariables = new HashMap<>();
+
+        uriVariables.put("id",workerId.toString());
+
+        Worker worker = restTemplate.getForObject(
+                workerHost + "/workers/{id}"
+                ,Worker.class
+                ,uriVariables);
+
+        return new Payment(worker.getName(), worker.getDailyIncome(), days); //temporary mock
     }
 
 
