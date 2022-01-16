32f339f44f82e57886eeead2eb87cd8d35d3811d
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2022-01-16 19:23:35 -0300

    Eureka Client Configuration

--- Payroll/src/main/java/com/github/marcelomachadoxd/hrpayrollms/PayrollApplication.java
+++ Payroll/src/main/java/com/github/marcelomachadoxd/hrpayrollms/PayrollApplication.java
@@ -2,10 +2,10 @@ package com.github.marcelomachadoxd.hrpayrollms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name="hrworkerms")
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class PayrollApplication {
