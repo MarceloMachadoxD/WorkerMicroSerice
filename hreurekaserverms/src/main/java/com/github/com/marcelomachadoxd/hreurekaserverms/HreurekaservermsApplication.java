66754ae7c3cb925f803754807af7366aae68cadf
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2022-01-16 17:59:14 -0300

    Project Eureka Server Created

---
+++ hreurekaserverms/src/main/java/com/github/com/marcelomachadoxd/hreurekaserverms/HreurekaservermsApplication.java
@@ -0,0 +1,13 @@
+package com.github.com.marcelomachadoxd.hreurekaserverms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HreurekaservermsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HreurekaservermsApplication.class, args);
+    }
+
+}
