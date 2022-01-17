988f5fc36872bef4b60120598f44df7469a1414c
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2022-01-17 20:14:13 -0300

    Hystrix

--- Payroll/src/main/java/com/github/marcelomachadoxd/hrpayrollms/resources/PaymentResource.java
+++ Payroll/src/main/java/com/github/marcelomachadoxd/hrpayrollms/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.github.marcelomachadoxd.hrpayrollms.resources;
 
 import com.github.marcelomachadoxd.hrpayrollms.entities.Payment;
 import com.github.marcelomachadoxd.hrpayrollms.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ public class PaymentResource {
     @Autowired
     private PaymentService paymentService;
 
+    @HystrixCommand(fallbackMethod = "getPaymentFallback")
     @GetMapping("/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(
             @PathVariable Long workerId,
@@ -24,5 +26,12 @@ public class PaymentResource {
         Payment payment = paymentService.getPayment(workerId, days);
         return ResponseEntity.ok(payment);
     }
+
+    public ResponseEntity<Payment> getPaymentFallback(
+            @PathVariable Long workerId,
+            @PathVariable Integer days){
+        return ResponseEntity.ok(new Payment("Error processing payment ", 0.00d,  0));
+    }
+
 
 }
