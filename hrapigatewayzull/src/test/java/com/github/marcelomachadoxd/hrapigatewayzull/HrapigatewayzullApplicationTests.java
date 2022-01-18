38d817172cbba3c6ddcfc48d2ae6876084b9a29f
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2022-01-18 19:45:05 -0300

    Project hr-api-gateway-zuul created

---
+++ hrapigatewayzull/src/test/java/com/github/marcelomachadoxd/hrapigatewayzull/HrapigatewayzullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcelomachadoxd.hrapigatewayzull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrapigatewayzullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
