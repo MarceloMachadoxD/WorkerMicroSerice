951e989ddec150686e6354574e01dd6743deee09
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2022-01-18 20:51:07 -0300

    Project HRConfigServer created

---
+++ hrconfigserverms/src/main/java/com/github/marcelomachadoxd/hrconfigserverms/HrconfigservermsApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcelomachadoxd.hrconfigserverms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrconfigservermsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrconfigservermsApplication.class, args);
+    }
+
+}
---
+++ hrconfigserverms/src/test/java/com/github/marcelomachadoxd/hrconfigserverms/HrconfigservermsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcelomachadoxd.hrconfigserverms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrconfigservermsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
